7ac1bbc6fa487dd91574e9bf8078a6d805a6ef2c
yousabbbbb <yousabmenissy@gmail.com>
2025-02-21 22:26:31 +0200

    Updated Formatting

--- mkdir/data.s
+++ mkdir/data.s
@@ -1,24 +1,23 @@
 .section .data
-    mflag: 
-        .string "-m"
-        .string "--mode"
-        .quad 0
+mflag:
+.string "-m"
+.string "--mode"
+.quad 0
 
-    hflags:
-        .string "-h"
-        .string "--help"
-        .quad 0
+hflags:
+.string "-h"
+.string "--help"
+.quad 0
 
-    usage: 
-        .ascii "Usage: mkdir <dirname>... [-m | --mode]\n"
-        .ascii "     Create the DIRECTORY(ies), if they do not already exist.\n\n"
-        .ascii "     Options:\n"
-        .ascii "     -m, --mode     set file mode. default is 0777\n"
-        .ascii "     -h, --help      show this message\n\n"
-        .ascii "     this is a simple assembly clone of the gnu mkdir utility.\n"
-        .ascii "     It's meant to serve as a working example for how to use the aslib library\n\0"
-    usage_len: .quad . - usage
+usage:
+.ascii "Usage: mkdir <dirname>... [-m | --mode]\n"
+.ascii "     Create the DIRECTORY(ies), if they do not already exist.\n\n"
+.ascii "     Options:\n"
+.ascii "     -m, --mode     set file mode. default is 0777\n"
+.ascii "     -h, --help      show this message\n\n"
+.ascii "     this is a simple assembly clone of the gnu mkdir utility.\n"
+.ascii "     It's meant to serve as a working example for how to use the aslib library\n\0"
+usage_len: .quad . - usage
 
-    modeErr: .string "mkdir: invalid mode '{s}'\n"
-    mkdirErr: .string "mkdir: could not create directory '{s}'\n"
-    
+modeErr: .string "mkdir: invalid mode '{s}'\n"
+mkdirErr: .string "mkdir: could not create directory '{s}'\n"
--- rm/data.s
+++ rm/data.s
@@ -1,46 +1,46 @@
 .section .data
-    veflags:
-        .string "-v"
-        .string "--verbose"
-        .quad   0
+veflags:
+.string "-v"
+.string "--verbose"
+.quad 0
 
-    hflags: 
-        .string "-h"
-        .string "--help"
-        .quad   0
+hflags:
+.string "-h"
+.string "--help"
+.quad 0
 
-    rflags:
-        .string "-r"
-        .string "--recursive"
-        .quad   0
+rflags:
+.string "-r"
+.string "--recursive"
+.quad 0
 
-    fflags:
-        .string "-f"
-        .string "--force"
-        .quad   0
+fflags:
+.string "-f"
+.string "--force"
+.quad 0
 
-    bflags:
-        .string "-v"
-        .string "--verbose"
-        .string "-h"
-        .string "--help"
-        .string "-r"
-        .string "--recursive"
-        .string "-f"
-        .string "--force"
-        .quad   0
-    
-    VFLAG: .byte 0
-    RFLAG: .byte 0
-    FFLAG: .byte 0
-    log_rm: .string "removed"
-    log_rm_len: .quad .-log_rm
-    
-    errFailure: .string "rm: could not remove"
-    errFailure_len: .quad .-errFailure
+bflags:
+.string "-v"
+.string "--verbose"
+.string "-h"
+.string "--help"
+.string "-r"
+.string "--recursive"
+.string "-f"
+.string "--force"
+.quad 0
 
-    usage: .string "Usage: rm [options] file...\n"
-    usage_len: .quad . - usage
+VFLAG: .byte 0
+RFLAG: .byte 0
+FFLAG: .byte 0
+log_rm: .string "removed"
+log_rm_len: .quad .-log_rm
 
- .section .bss
-    .lcomm statbuff, 144
+errFailure: .string "rm: could not remove"
+errFailure_len: .quad .-errFailure
+
+usage: .string "Usage: rm [options] file...\n"
+usage_len: .quad . - usage
+
+.section .bss
+.lcomm statbuff, 144
